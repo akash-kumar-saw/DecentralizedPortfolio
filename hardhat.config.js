2b2f1544e103d881f20e88ac778d94c488781b1e
Akash Kumar Saw <akashkumarsaw03@gmail.com>
2023-08-12 02:17:12 +0530

    Updated hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,17 +6,18 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {}, 
+    sophia: {
+      url: "https://rpc.ankr.com/eth_sepolia",
+      accounts: [`0xYOUR_SEPOLIA_PRIVATE_KEY`],
+    },
     /*mainnet: {
       url: "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID",
       accounts: [`0xYOUR_MAINNET_PRIVATE_KEY`],
-    },
-    sophia: {
-      url: "http://sepolia.example.com",
-      accounts: [`0xYOUR_SEPOLIA_PRIVATE_KEY`],
     },*/
+    
   },
 
   paths: {
-    artifacts: "./client/src/artifacts",
+    artifacts: "./artifacts",
   },
 };
