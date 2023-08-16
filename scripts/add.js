18a9bd46f65524e6f8673ed8da9b48e6540cb24b
Akash Kumar Saw <akashkumarsaw03@gmail.com>
2023-08-17 03:09:15 +0530

    Updated scripts/add,read.js

--- scripts/add.js
+++ scripts/add.js
@@ -1,7 +1,7 @@
 const ABI = require('./ABI.json');
 const ethers = require('ethers');
 
-const contractAddress = '0x7166310C7023F1b8b4bcf7804992EdFD4D079591';
+const contractAddress = '0x5441536C344f1023f8C970c3E39AB6C4Fe39D938';
 
 const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth_sepolia');
 
@@ -9,9 +9,10 @@ const signer = new ethers.Wallet('0xYOUR_PRIVATE_KEY', provider);
 const contract = new ethers.Contract(contractAddress, ABI, signer);
 
 async function addHero() {
-    const title = "DevOps & Blockchain";
-    const subtitle = "Engineer";
-    const image = "QmXgjnS7MLSB2sdmGUtCv588FBw6QvuttfFne7A31h8X1k/Hero.png";
+    const title = ""
+    const subtitle = ""
+    const image = ""
+
     console.log('Adding Hero...');
     const transactionResponse = await contract.addHero(title, subtitle, image);
     console.log('Hero Added Successfully!');
@@ -19,8 +20,9 @@ async function addHero() {
 }
 
 async function addHeroContent() {
-    const count = "10+";
-    const text = "Proficient in coding across a spectrum of 10+ languages";
+    const count = ""
+    const text = ""
+
     console.log('Adding Hero Content...');
     const transactionResponse = await contract.addHeroContent(count, text);
     console.log('Hero Content Added Successfully!');
@@ -28,19 +30,8 @@ async function addHeroContent() {
 }
 
 async function addSkill() {
-    const name = "BlockChain Developer"
-    const skills = [
-        "JavaScript",
-        "Python",
-        "Web3.js",
-        "Ethers.js",
-        "Web3.py",
-        "Solidity",
-        "ChainLink/Oracle", 
-    ]
-    while (skills.length < 30) {
-        skills.push(" ");
-    }
+    const name = ""
+    const skills = ""
     
     console.log('Adding Skill...');
     const transactionResponse = await contract.addSkill(name, skills);
@@ -48,10 +39,68 @@ async function addSkill() {
     console.log('Transaction Hash:', transactionResponse.hash);
 }
 
+async function addService() {
+    const title = ""
+    const para = ""
+    
+    console.log('Adding Service...');
+    const transactionResponse = await contract.addService(title, para);
+    console.log('Service Added Successfully!');
+    console.log('Transaction Hash:', transactionResponse.hash);
+}
+
+async function addProject() {
+    const title = ""
+    const image = ""
+    const githubLink = ""
+    
+    console.log('Adding Project...');
+    const transactionResponse = await contract.addProject(title, image, githubLink);
+    console.log('Project Added Successfully!');
+    console.log('Transaction Hash:', transactionResponse.hash);
+}
+
+async function addTestimonial() {
+    const review = ""
+    const image = ""
+    const designation = ""
+    
+    console.log('Adding Testimonial...');
+    const transactionResponse = await contract.addTestimonial(review, image, designation);
+    console.log('Testimonial Added Successfully!');
+    console.log('Transaction Hash:', transactionResponse.hash);
+}
+
+async function addHireme() {
+    const para = ""
+    const image = ""
+    
+    console.log('Adding Hireme...');
+    const transactionResponse = await contract.addHireme(para, image);
+    console.log('Hireme Added Successfully!');
+    console.log('Transaction Hash:', transactionResponse.hash);
+}
+
+async function addContact() {
+    const email = ""
+    const whatsapp = ""
+    const github = ""
+    
+    console.log('Adding Contact...');
+    const transactionResponse = await contract.addContact(email, whatsapp, github);
+    console.log('Contact Added Successfully!');
+    console.log('Transaction Hash:', transactionResponse.hash);
+}
+
 async function main() {
-    await addHero();
-    await addHeroContent();
-    await addSkill();
+    // await addHero();
+    // await addHeroContent();
+    // await addSkill();
+    // await addService();
+    // await addProject();
+    // await addTestimonial();
+    // await addHireme();
+    // await addContact();
 }
 
 main();
--- scripts/read.js
+++ scripts/read.js
@@ -1,7 +1,7 @@
 const ABI = require('./ABI.json');
 const ethers = require('ethers');
 
-const contractAddress = '0x7166310C7023F1b8b4bcf7804992EdFD4D079591';
+const contractAddress = '0x5441536C344f1023f8C970c3E39AB6C4Fe39D938';
 
 const provider = new ethers.providers.JsonRpcProvider('https://rpc.ankr.com/eth_sepolia');
 
@@ -11,39 +11,64 @@ const contract = new ethers.Contract(contractAddress, ABI, signer);
 
 async function main () {
     try {
-        console.log('Loading Hero...');
-        const hero = await contract.hero();
-        console.log('Hero:\n', hero);
+        console.log('**********Loading Hero**********');
+        const content = await contract.hero();
+        console.log('Hero:\n', content);
 
-        console.log('Loading Hero Content...');
-        const heroContent = await contract.heroContent(0);
+        console.log('**********Loading Hero Content**********');
+        const heroContent = [];
+        var contentCount = await contract.heroCount();
+        for (let i = 0; i < contentCount; i++) {
+          const content = await contract.heroContent(i);
+          heroContent.push(content);
+        }
         console.log('Hero Content:\n', heroContent);
 
 
-        console.log('Loading Skills...');
-        const skills = await contract.skills(0);
+        console.log('**********Loading Skills**********');
+        const skills = [];
+        contentCount = await contract.skillsCount();
+        for (let i = 0; i < contentCount; i++) {
+          const content = await contract.skills(i);
+          skills.push(content);
+        }
         console.log('Skills:\n', skills);
 
 
-        console.log('Loading Services...');
-        const services = await contract.services(0);
+        console.log('**********Loading Services**********');
+        const services = [];
+        contentCount = await contract.servicesCount();
+        for (let i = 0; i < contentCount; i++) {
+          const content = await contract.services(i);
+          services.push(content);
+        }
         console.log('Services:\n', services);
 
-        console.log('Loading Projects...');
-        const projects = await contract.projects(0);
+        console.log('**********Loading Projects**********');
+        const projects = [];
+        contentCount = await contract.projectsCount();
+        for (let i = 0; i < contentCount; i++) {
+          const content = await contract.projects(i);
+          projects.push(content);
+        }
         console.log('Services:\n', projects);
 
-        console.log('Loading Testimonials...');
-        const testimonials = await contract.testimonials(0);
+        console.log('**********Loading Testimonials**********');
+        const testimonials = [];
+        contentCount = await contract.testimonialsCount();
+        for (let i = 0; i < contentCount; i++) {
+          const content = await contract.testimonials(i);
+          testimonials.push(content);
+        }
         console.log('Testimonials:\n', testimonials);
 
 
-        console.log('Loading Hire Me...');
+        console.log('**********Loading Hire Me**********');
         const hireme = await contract.hireme();
         console.log('Hire Me:\n', hireme);
 
 
-        console.log('Loading Contact...');
+        console.log('**********Loading Contact**********');
         const contact = await contract.contact();
         console.log('Contact:\n', contact);     
     } catch (error) {
